b419b503b15a7abc49a23615812c7e48d55a8e49
ben-ayasan <benjamin@azbcsit.com>
2024-05-17 14:49:38 -0700

    commit 4

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # proyecto_sprint_5
 Proyecto Sprint5
+
+Incluir una breve descripción del proyecto.
+
--- app.py
+++ app.py
@@ -3,29 +3,20 @@ import pandas as pd
 import plotly_express as px
 
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
-hist_button = st.button(
-    'Haz click aqui para construir histograma')  # crear un botón
+# crear un botón
 
-st.header('Informacion Importante')
-if hist_button:  # al hacer clic en el botón
-    # escribir un mensaje
-    st.write(
-        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+st.header('Informacion de kilometraje de vehiculos')
+if hist_button:
 
-    # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
-
-    # mostrar un gráfico Plotly interactivo
+build_histogram = st.checkbox('Construir un histograma')
+# al hacer clic en el botón
+build_scatter = st.checkbox('Construir un diagrama de dispersión')
+# escribir un mensaje
+if build_histogram:  # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+    fig = px.histogram(car_data, x='odometer')
     st.plotly_chart(fig, use_container_width=True)
-
-disp_button = st.button(
-    'Haz click aqui para construir grafica de dispersión')
-st.header('Informacion con grafico de dispersión ')
-if hist_button:  # al hacer clic en el botón
-
-    st.write('Creación de grafico de dispersión para el conjunto de datos de anuncios de venta de coches')
-
-# crear un gráfico de dispersión
-fig = px.scatter(car_data, x="odometer", y="price")
-fig.show()  # crear gráfico de dispersión
-st.plotly_chart(fig, use_container_width=True)
+elif build_scatter:
+    st.write('Construir un diagrama de dispersión de precios y kilometraje')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig)
